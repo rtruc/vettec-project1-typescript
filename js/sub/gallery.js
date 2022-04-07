ac082932d7191a398852811eaf64fa175ed9b231
rtruc <ubno@me.com>
2022-04-06 23:22:20 -0400

    CSV parsing for image gallery working

--- js/sub/gallery.js
+++ js/sub/gallery.js
@@ -1,38 +1,51 @@
 // All images should be sized appropriately and have alternative text displayed should the image fail to render
 
-const CSVTest = ['jordan-aloft.jpeg,Alt Text, Title Text', 'jordan-aloft.jpeg,Alt Text, Title Text'];
+const CSVTest = ['jordan-aloft.jpeg,Alt Text, Title Text', 'jordan-aloft-2.jpeg,Alt Text2, Title Text2'];
 
 function parseCSVPhotoData(csv){
     let photos = [];
-    let photo = {};
+    splitsStrings = csv.split(/\r?\n/);
+    // splitsStrings = String(csv);
 
-    for(let entry of csv) {
-        let txt = "";
-        txt = csv;
-        console.log(txt);
-        
-        let splits = [""];
-        splits = txt.split(',');
+    for(let entry of splitsStrings) {
+        splits = entry.split(',');
         console.log(splits);
-    
+        
+        let photo = {};
         photo.fileName = splits[0].trim();
         photo.altText = splits[1].trim();
         photo.titleText = splits[2].trim();
         
         console.log("FileName: " + photo.fileName);
-        photos.push(phot0o);
+        photos.push(photo);
     }
-
 
     return photos;
 }
 
-// console.log(CSVTest);
-let parsedPhoto = parseCSVPhotoData(CSVTest)
-let photo = parsedPhoto.pop();
-console.log(photo);
+function readFile() {
 
-function generatePhotoRowAlt(photos, folder) {
+    fetch('./img/staging/Training/Training.csv')
+        .then(response => response.text())
+        .then(responseText => parseCSVPhotoData(responseText))
+        // .then(parsedStrings => console.log(parsedStrings))
+        .then(photoStrings => generatePhotoColumn(photoStrings, "./img/staging/Training/"))
+        .then(photoSection => appendPhotoSectionToGallery(photoSection))
+        .then(enableClickToZoomOnImages())
+        .catch(console.log("Oh shit!"));
+}
+
+// let parsedPhoto = parseCSVPhotoData(CSVTest)
+// let photo = parsedPhoto.pop();
+// console.log(photo);
+
+function appendPhotoSectionToGallery(photoSection) {
+    let galleryDiv = document.getElementById('gallery');
+    // console.log(photoRowDiv);
+    galleryDiv.appendChild(photoSection);
+}
+
+function generatePhotoRow(photos, folder) {
     let rowDiv = document.createElement('div');
     // console.log(rowDiv);
     rowDiv.classList.add("flex-row");
@@ -49,9 +62,9 @@ function generatePhotoRowAlt(photos, folder) {
     return rowDiv;
 }
 
-function generatePhotoRow(photos, folder) {
+function generatePhotoRowAlt(photos, folder) {
     let rowDiv = document.createElement('div');
-    // console.log(rowDiv);
+    // console.log(folder+photos[0].fileName);
     rowDiv.classList.add("flex-row");
     
     for (let photo of photos) {
@@ -76,37 +89,72 @@ function generatePhotoColumn(photos, folder) {
     return columnDiv;
 }
 
+function zoomImage() {
+    // If image that was clicked on is already zoomed, unzoom and return
+    if (currentlyZoomedImage == this) {
+        currentlyZoomedImage.classList.remove("zoom");
+        currentlyZoomedImage = null;
+        return;
+    }
+
+    // If a different image is zoomed, unzoom that image and then zoom
+    // the image that was clicked on
+    if (currentlyZoomedImage) {
+        currentlyZoomedImage.classList.remove("zoom");
+    }
+    this.classList.add("zoom");
+    currentlyZoomedImage = this;
+
+    // How to center image?
+}
+
+function enableClickToZoomOnImages() {
+    images = document.getElementsByClassName("gallery-img");
+    for (let i = 0; i < images.length; i++) {
+        images[i] = images[i].addEventListener('click', zoomImage);
+    }
+}
 
 
+window.addEventListener('DOMContentLoaded', () => {
 
-// window.addEventListener('load', () => {
-//     const folder = './img/photos/';
-//     const photos = [
-//         'jordan-aloft.jpeg',
-//         'jordan-aloft-2.jpeg',
-//         'jordan-aloft-3.jpeg',
-//     ]
+})
+
+window.addEventListener('load', () => {
+    // const folder = './img/photos/';
+    // const photos = [
+    //     'jordan-aloft.jpeg',
+    //     'jordan-aloft-2.jpeg',
+    //     'jordan-aloft-3.jpeg',
+    // ]
+
+    readFile();
+
+    // let fetchData = await readFile();
+    // console.log(fetchData);
+
+    // let galleryDiv = document.getElementById('gallery');
+    // let photoRowDiv = readFile();
+    // console.log(photoRowDiv);
+    // galleryDiv.appendChild(photoRowDiv);
 
 
+    // console.log(galleryDiv);
+    // // generatePhotoRow(photos, folder);
+    // let photoRowDiv = generatePhotoColumn(photos, folder);
+    // // console.log("photo row div:" + photoRowDiv);
+    // // galleryDiv.innerHTML = photoRowDiv;
+    // console.log(galleryDiv);
+    // galleryDiv.innerHTML = generatePhotoRow(photos, folder);
+    // console.log(galleryDiv);
+    // console.log(galleryDiv);
+    // console.log("Fudge");
 
-//     let galleryDiv = document.getElementById('gallery');
-//     console.log(galleryDiv);
-//     // generatePhotoRow(photos, folder);
-//     let photoRowDiv = generatePhotoColumn(photos, folder);
-//     // console.log("photo row div:" + photoRowDiv);
-//     galleryDiv.appendChild(photoRowDiv)
-//     // galleryDiv.innerHTML = photoRowDiv;
-//     console.log(galleryDiv);
-//     // galleryDiv.innerHTML = generatePhotoRow(photos, folder);
-//     // console.log(galleryDiv);
-//     // console.log(galleryDiv);
-//     // console.log("Fudge");
-
-//     // images = document.getElementsByClassName("gallery-img");
-//     // for (let i = 0; i < images.length; i++) {
-//     //     images[i] = images[i].addEventListener('click', zoomImage);
-//     // }
-//     // topButton = document.getElementById('active');
-//     // console.log(topButton);
-//     // topButton.addEventListener('click', scrollToTop);
-// })
+    // images = document.getElementsByClassName("gallery-img");
+    // for (let i = 0; i < images.length; i++) {
+    //     images[i] = images[i].addEventListener('click', zoomImage);
+    // }
+    // topButton = document.getElementById('active');
+    // console.log(topButton);
+    // topButton.addEventListener('click', scrollToTop);
+})
--- js/sub/script.js
+++ js/sub/script.js
@@ -9,24 +9,24 @@ function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
-function zoomImage() {
-    // If image that was clicked on is already zoomed, unzoom and return
-    if (currentlyZoomedImage == this) {
-        currentlyZoomedImage.classList.remove("zoom");
-        currentlyZoomedImage = null;
-        return;
-    }
+// function zoomImage() {
+//     // If image that was clicked on is already zoomed, unzoom and return
+//     if (currentlyZoomedImage == this) {
+//         currentlyZoomedImage.classList.remove("zoom");
+//         currentlyZoomedImage = null;
+//         return;
+//     }
 
-    // If a different image is zoomed, unzoom that image and then zoom
-    // the image that was clicked on
-    if (currentlyZoomedImage) {
-        currentlyZoomedImage.classList.remove("zoom");
-    }
-    this.classList.add("zoom");
-    currentlyZoomedImage = this;
+//     // If a different image is zoomed, unzoom that image and then zoom
+//     // the image that was clicked on
+//     if (currentlyZoomedImage) {
+//         currentlyZoomedImage.classList.remove("zoom");
+//     }
+//     this.classList.add("zoom");
+//     currentlyZoomedImage = this;
 
-    // How to center image?
-}
+//     // How to center image?
+// }
 
 
 
@@ -38,10 +38,10 @@ function zoomImage() {
 // })
 
 window.addEventListener('load', () => {
-    images = document.getElementsByClassName("gallery-img");
-    for (let i = 0; i < images.length; i++) {
-        images[i] = images[i].addEventListener('click', zoomImage);
-    }
+    // images = document.getElementsByClassName("gallery-img");
+    // for (let i = 0; i < images.length; i++) {
+    //     images[i] = images[i].addEventListener('click', zoomImage);
+    // }
     topButton = document.getElementById('active');
     // console.log(topButton);
     topButton.addEventListener('click', scrollToTop);
